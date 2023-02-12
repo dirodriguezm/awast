61c45df09b412f82ebff306f92a8f5d0df054a7e
Diego <diegorodriguezmancini@gmail.com>
2023-02-12 14:06:37 -0300

    refactor: create deployment with single values file

--- awast/cli/kubernetes/new_deployment.py
+++ awast/cli/kubernetes/new_deployment.py
@@ -1,39 +1,48 @@
-from awast.cli.utils import create_file, merge_values, get_default_values
+from awast.cli.utils import (
+    CreateFileArguments,
+    create_file,
+    create_file_arguments,
+    merge_values,
+    get_default_values,
+    parse_default_values,
+)
 import pathlib
 from typing import List, Callable
 
 
-def new_deployment(name: str, values: List[str], value_parser: Callable):
-    base = pathlib.Path.cwd()
-    output_path = base / name
-    if output_path.exists():
-        raise Exception("Output directory already exist.")
-    parsed_values = value_parser(values)
-    create_deployment_file(name, output_path, parsed_values)
-
-
-def create_deployment_file(
-    deployment_name: str,
-    output_path: pathlib.Path,
-    values: dict,
+def new_deployment(
+    name: str, values: List[str], value_parser: Callable, values_file: str
 ):
-    if not (output_path / "deployment").exists():
-        (output_path / "deployment").mkdir()
-
     module_path = pathlib.Path(__file__).parent.resolve()
     awast_path = (module_path / "../..").resolve()
     template_path = (awast_path / "templates/kubernetes").absolute()
 
-    values = merge_values(
-        get_default_values(template_path / "values.yaml"),
-        values,
-    )
-    values["name"] = deployment_name
+    base = pathlib.Path.cwd()
+    output_path = base / name
 
+    if not (output_path / "deployment").exists():
+        (output_path / "deployment").mkdir()
+
+    parsed_values = value_parser(values)
+    parsed_values = parse_default_values(
+        template_path, values_file, parsed_values
+    )
+    parsed_values["name"] = name
+
+    arguments = create_file_arguments(
+        name, output_path, parsed_values, template_path, "deployment"
+    )
+
+    create_deployment_file(arguments)
+
+
+def create_deployment_file(arguments: Callable[[], CreateFileArguments]):
+    args = arguments()
+    print(args)
     create_file(
-        template_path=template_path,
+        template_path=args.template_path,
         template_name="deployment.jinja",
-        app_path=output_path,
+        app_path=args.output_path,
         filename="deployment/deployment.yaml",
-        **values,
+        **args.parsed_values,
     )
--- tests/unit/cli/test_new_deployment.py
+++ tests/unit/cli/test_new_deployment.py
@@ -3,11 +3,21 @@ from awast.cli.kubernetes.new_deployment import (
     new_deployment,
     create_deployment_file,
 )
-from awast.cli.utils import value_parser
+from awast.cli.utils import (
+    CreateFileArguments,
+    create_file_arguments,
+    value_parser,
+)
 import pathlib
+from pytest_mock import MockerFixture
 
 
-def test_new_deployment_with_values(mocker):
+TESTS_PATH = pathlib.Path.cwd() / "tests"
+TESTS_OUTPUT_FILES = TESTS_PATH / "unit" / "cli" / "output_files"
+TEMPLATE_PATH = (pathlib.Path.cwd() / "awast/templates/kubernetes").resolve()
+
+
+def test_new_deployment_with_values(mocker: MockerFixture):
     create_deployment_file_mock = mocker.patch(
         "awast.cli.kubernetes.new_deployment.create_deployment_file"
     )
@@ -15,24 +25,45 @@ def test_new_deployment_with_values(mocker):
     getcwd.return_value = pathlib.Path("/tmp")
     mocker.patch.object(tpathlib.Path, "mkdir")
     new_deployment(
-        "test-api", ["version=1", "resources.requests.cpu=10m"], value_parser
-    )
-    create_deployment_file_mock.assert_called_with(
         "test-api",
-        pathlib.Path("/tmp/test-api"),
-        {"version": "1", "resources": {"requests": {"cpu": "10m"}}},
+        [
+            "resources.requests.cpu=10m",
+            "image.name=test",
+            "nodegroup=test",
+        ],
+        value_parser,
+        "",
     )
+    create_deployment_file_mock.assert_called()
+    call_args = create_deployment_file_mock.call_args[0][0]
+    assert call_args().name == "test-api"
+    assert call_args().parsed_values == {
+        "name": "test-api",
+        "resources": {"requests": {"cpu": "10m", "memory": None}},
+        "image": {"name": "test", "version": "latest"},
+        "containerPort": 8000,
+        "namespace": "web-services",
+        "nodegroup": "test",
+    }
 
 
 def test_create_deployment_file(tmp_path):
-    (tmp_path / "test-api").mkdir()
+    (tmp_path / "test-api" / "deployment").mkdir(parents=True)
     values = {
-        "environment": "test",
-        "version": "1",
-        "port": "8000",
-        "resources": {"requests": {"mem": "10M", "cpu": "10m"}},
+        "name": "test-api",
+        "containerPort": "8000",
+        "resources": {"requests": {"memory": "10M", "cpu": "10m"}},
+        "nodegroup": "test-nodegroup",
+        "image": {"name": "test-api", "version": "latest"},
     }
-    create_deployment_file("test-api", tmp_path / "test-api", values)
+    arguments = create_file_arguments(
+        "test-api",
+        tmp_path / "test-api",
+        values,
+        TEMPLATE_PATH,
+        "deployment",
+    )
+    create_deployment_file(arguments)
     assert_deployment_values(
         tmp_path / "test-api" / "deployment" / "deployment.yaml"
     )
@@ -41,8 +72,8 @@ def test_create_deployment_file(tmp_path):
 def assert_deployment_values(path):
     text = path.read_text()
     assert "name: test-api" in text
-    assert "image: ghcr.io/alercebroker/test-api:1" in text
-    assert "test-web-services" in text
+    assert "image: ghcr.io/alercebroker/test-api:latest" in text
+    assert "test-nodegroup" in text
     assert "containerPort: 8000" in text
     assert "memory: 10M" in text
     assert "cpu: 10m" in text
